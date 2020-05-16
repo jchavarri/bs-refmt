46c245146533963cb0bda78f23e30889fbac2aa9
Javier Chávarri <javier.chavarri@gmail.com>
2020-05-17 01:26:01 +0200

    fix module name

--- src/Refmt_api.ml
+++ src/Refmt_api.ml
@@ -1,6 +1,6 @@
 module RE = struct
-  include Reason_toolchain.RE
+  include Reason_toolchain_packed.Reason_toolchain.RE
 end
 module ML = struct
-  include Reason_toolchain.ML
+  include Reason_toolchain_packed.Reason_toolchain.ML
 end
